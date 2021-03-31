0cb1907ed1030c78e1ce82710b15bfab4a2297bc
Basma11elmihy <basma.elmihy@gmail.com>
2021-03-31 20:53:18 +0200

    Initial commit

---
+++ Meditreks.js
@@ -0,0 +1,196 @@
+// Counter Section:
+$(function($, win) {
+    $.fn.inViewport = function(cb) {
+      return this.each(function(i,el){
+        function visPx(){
+          var H = $(this).height(),
+              r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+          return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));  
+        } visPx();
+        $(win).on("resize scroll", visPx);
+      });
+    };
+  }(jQuery, window));
+
+
+jQuery(function($) {
+
+    $(".section5-heading").inViewport(function(px) { 
+      if(px>0 && !this.initNumAnim) { 
+        this.initNumAnim = true; 
+        $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+        }, {
+          duration: 1500,
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });         
+      }
+    });
+  
+  });
+
+
+// Testimonials Section 
+$(document).ready(function(){
+  $('#pic-one').click(function(){
+    $("#bubble").removeClass( "left" ).addClass( "right" );
+    $("#bubble").css("top","-5%");
+    $("#bubble").css("right","37%");
+    $("#bubble-text").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+    $("#bubble-title").text("Jasi Toor");
+    $("#bubble-sub-title").text("CEO");  
+  })
+});
+
+$(document).ready(function(){
+  $('#pic-two').click(function(){
+    $("#bubble").removeClass( "right" ).addClass( "left" );
+    $("#bubble").css("top","16%");
+    $("#bubble").css("right","28.5%");
+    $("#bubble-text").text("Extensive delivery proofs, intelligent AI location identification, reciepent access protocols We've built it all in!");
+    $("#bubble-title").text("Muhammed Maksoud");
+    $("#bubble-sub-title").text("Product Designer");  
+  })
+});
+
+$(document).ready(function(){
+  $('#pic-three').click(function(){
+    $("#bubble").removeClass( "left" ).addClass( "right" );
+    $("#bubble").css("top","15%");
+    $("#bubble").css("right","17%");
+    $("#bubble-text").text("Extensive delivery proofs, intelligent AI location identification, reciepent access protocols We've built it all in!");
+    $("#bubble-title").text("Basma Elmihy");
+    $("#bubble-sub-title").text("Operation Manager");  
+  })
+});
+
+$(document).ready(function(){
+  $('#pic-four').click(function(){
+    $("#bubble").removeClass( "right" ).addClass( "left" );
+    $("#bubble").css("top","36%");
+    $("#bubble").css("right","11%");
+    $("#bubble-text").text("Extensive delivery proofs, intelligent AI location identification, reciepent access protocols We've built it all in!");
+    $("#bubble-title").text("Basma Elmihy");
+    $("#bubble-sub-title").text("Operation Manager");  
+  })
+});
+
+$(document).ready(function(){
+  $('#pic-five').click(function(){
+    $("#bubble").removeClass( "left" ).addClass( "right" );
+    $("#bubble").css("top","36%");
+    $("#bubble").css("right","43%");
+    $("#bubble-text").text("Extensive delivery proofs, intelligent AI location identification!");
+    $("#bubble-title").text("Basma Elmihy");
+    $("#bubble-sub-title").text("Customer Support Specialist");  
+  })
+});
+
+$(document).ready(function(){
+  $('#pic-six').click(function(){
+    $("#bubble").removeClass( "right" ).addClass( "left" );
+    $("#bubble").css("top","58%");
+    $("#bubble").css("right","38%");
+    $("#bubble-text").text("Extensive delivery proofs, intelligent AI location identification, reciepent access protocols We've built it all in! Extensive delivery proofs, intelligent AI location identification, reciepent access protocols We've built it all in!");
+    $("#bubble-title").text("Basma Elmihy");
+    $("#bubble-sub-title").text("Front-End Developer");  
+  })
+});
+
+$(document).ready(function(){
+  $('#Monthly').click(function(){
+    $('#pills-slider').css("transform","translateX(5px)");
+    $(this).css("color","white");
+    $('#Annually').css("color","black");
+
+    $('#delivery-price-sub').text("$19.99");
+    $('#delivery-number-sub').text("/ PER MONTH");
+    $('#discount').css("visibility",'hidden');
+    
+  })
+  $('#Annually').click(function(){
+    $('#pills-slider').css("transform","translateX(205px)");
+    $(this).css("color","white");
+    $('#Monthly').css("color","black");
+
+    $('#delivery-price-sub').text("$199.99");
+    $('#delivery-number-sub').text("/ PER YEAR");
+    $('#discount').css("visibility",'visible');
+  })
+});
+
+$(document).ready(function(){
+  var slider = document.getElementById("myRange");
+  var currentVal = slider.value;
+
+   
+  slider.oninput = () => {
+    if (slider.value >= 55 && slider.value < 75){
+        $('#image-3').css('transform','scale(1.5)');
+        $('#image-2').css('transform','scale(0.9)');
+        
+        $('#delivery-price').css('opacity','0.5');
+        $('#delivery-number').css('opacity','0.5');
+
+       } 
+      else if (slider.value >= 75 && slider.value < 90){
+        $('#image-3').css('transform','scale(1.7)');
+        $('#image-2').css('transform','scale(0.7)');
+
+        $('#delivery-price').css('opacity','0.5');
+        $('#delivery-number').css('opacity','0.5');
+
+        $('#delivery-price').text('$5');
+        $('#delivery-number').text('/ MORE THAN 1000 ORDERS');
+       }
+       else if (slider.value >= 90 && slider.value <= 100){
+        $('#image-3').css('transform','scale(2)');
+        $('#image-2').css('transform','scale(0.6)');
+        $('#delivery-price').css('opacity','1');
+        $('#delivery-number').css('opacity','1');
+
+        }
+        else if (slider.value >= 50 && slider.value < 55){
+          $('#image-1').css('transform','scale(0.9)');
+          $('#image-3').css('transform','scale(0.9)');
+          $('#image-2').css('transform','scale(1.3)');
+
+          $('#delivery-price').css('opacity','1');
+        $('#delivery-number').css('opacity','1');
+        $('#delivery-price').text('$7');
+        $('#delivery-number').text('/ LESS THAN 100 ORDERS');
+        } 
+
+        else if (slider.value < 45 && slider.value > 35){
+          $('#image-1').css('transform','scale(1.5)');
+          $('#image-2').css('transform','scale(0.9)');
+
+          $('#delivery-price').css('opacity','0.5');
+        $('#delivery-number').css('opacity','0.5');
+
+          
+  
+         } 
+        else if (slider.value <= 35 && slider.value > 10){
+          $('#image-1').css('transform','scale(1.7)');
+          $('#image-2').css('transform','scale(0.7)');
+          $('#delivery-price').text('$6');
+        $('#delivery-number').text('/ MORE THAN 100 ORDERS');
+         }
+         else if (slider.value <= 10 && slider.value >= 0){
+          $('#image-1').css('transform','scale(2)');
+          $('#image-2').css('transform','scale(0.5)');
+
+          $('#delivery-price').css('opacity','1');
+        $('#delivery-number').css('opacity','1');
+
+          }
+     
+       
+
+    } 
+    
+    
+  })
